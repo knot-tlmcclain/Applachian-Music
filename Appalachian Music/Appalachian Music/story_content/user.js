804729780f765c830695b05c855fc12e1c056d26
Tom McClain <90461522+jctmcclain@users.noreply.github.com>
2025-01-02 15:42:41 -0500

    Web Object
    Testing from Juniata College with Web Object enabled.

---
+++ Appalachian Music/Appalachian Music/story_content/user.js
@@ -0,0 +1,16 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const varInstNaem = "Guitar";
+const varInstImage = "guitar.jpg";
+const varInstNumberStrings = = "6";
+
+}
+
+};
